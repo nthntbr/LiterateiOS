8a4c539ec7f5168ca947bd617fd8fa7f5ca2f1f8
nthntbr <nathan.teuber@icloud.com>
2022-05-09 13:19:35 -0600

    Initial Commit

---
+++ Literate/LiterateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiterateApp.swift
+//  Literate
+//
+//  Created by Nathan Teuber on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LiterateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
